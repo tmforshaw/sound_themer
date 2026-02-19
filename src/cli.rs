f2e2f20e6c0fdaabaee3b621c6493df773ff57ca
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-02-19 18:01:33 +0000

    Add List command, as well as Play command for CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,12 @@
-use clap::Parser;
+use std::{fs, path::Path};
 
-use crate::config::get_config;
+use clap::{Parser, Subcommand};
+
+use crate::{
+    config::{get_config, get_theme_path_from_name, override_sound_ext, override_theme_name},
+    error::ThemerError,
+    sound::play_sound,
+};
 
 #[derive(Parser, Debug)]
 #[command(name = "sound_themer")]
@@ -15,8 +21,19 @@ pub struct Cli {
     #[arg(short, long, default_value_t = get_default_sound_ext())]
     pub ext: String,
 
-    /// The name of the sound which will be played (from the selected theme)
-    pub sound_name: String,
+    #[command(subcommand)]
+    pub commands: CliCommands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum CliCommands {
+    #[command(alias = "g", about = "Play a sound from the sound theme using a given sound name")]
+    Play {
+        /// The name of the sound which will be played (from the selected theme)
+        sound_name: String,
+    },
+    #[command(alias = "l", alias = "ls", about = "List the sounds in the sound theme")]
+    List,
 }
 
 fn get_default_theme_name() -> String {
@@ -25,4 +42,48 @@ fn get_default_theme_name() -> String {
 
 fn get_default_sound_ext() -> String {
     get_config().unwrap_or_else(|e| panic!("{e}")).sound_ext
+}
+
+pub fn evaluate_cli() -> Result<(), ThemerError> {
+    let cli = Cli::parse();
+
+    // Override config theme name with cli parsed theme name
+    override_theme_name(cli.theme)?;
+
+    // Override config sound extension with cli parsed sound extension
+    override_sound_ext(cli.ext)?;
+
+    match cli.commands {
+        CliCommands::Play { sound_name } => play_sound(sound_name)?,
+        CliCommands::List => {
+            // Get the theme path where the sound files are
+            let theme_path_str = format!("{}/stereo/", get_theme_path_from_name()?);
+
+            println!("Listing files in '{theme_path_str}':");
+
+            // Check this full path exists
+            let theme_path = Path::new(&theme_path_str);
+            if theme_path.exists() {
+                // Get all the files in this folder and convert to their file names
+                fs::read_dir(theme_path)
+                    .map_err(|e| ThemerError::FileReadError(e.to_string()))?
+                    .flatten()
+                    .filter_map(|entry| {
+                        let path = entry.path();
+
+                        if path.is_file() {
+                            path.file_name().map(|file_name| format!("{}", file_name.display()))
+                        } else {
+                            None
+                        }
+                    })
+                    // Then print them each on separate lines
+                    .for_each(|file| println!("\t{file}"));
+            } else {
+                return Err(ThemerError::ThemePathNotFoundError(theme_path_str));
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use clap::Parser;
-
-use crate::{
-    cli::Cli,
-    config::{override_sound_ext, override_theme_name},
-    sound::play_sound,
-};
+use crate::cli::evaluate_cli;
 
 pub mod cli;
 pub mod config;
@@ -12,19 +6,7 @@ pub mod error;
 pub mod sound;
 
 fn main() {
-    let cli = Cli::parse();
-
-    // Override config theme name with cli parsed theme name
-    if let Err(e) = override_theme_name(cli.theme) {
-        panic!("{e}");
-    }
-
-    // Override config sound extension with cli parsed sound extension
-    if let Err(e) = override_sound_ext(cli.ext) {
-        panic!("{e}");
-    }
-
-    if let Err(e) = play_sound(cli.sound_name) {
-        panic!("{e}");
-    }
+    if let Err(e) = evaluate_cli() {
+        eprintln!("{e}");
+    };
 }
