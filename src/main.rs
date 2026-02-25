c2ee08fee7f57d4291b96ca35887363d0be0bf3e
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-02-25 18:52:07 +0000

    Update Cargo, move public imports to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-#![warn(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-#![warn(clippy::unwrap_used)]
-#![warn(clippy::expect_used)]
-#![allow(clippy::cast_possible_truncation)]
-
-use crate::cli::evaluate_cli;
+use sound_themer::cli::evaluate_cli;
 
 // TODO Add modularity to sound playing so other commands can be used
-
-pub mod cli;
-pub mod config;
-pub mod duration;
-pub mod error;
-pub mod mapping;
-pub mod sound;
-pub mod theme;
 
 fn main() {
     if let Err(e) = evaluate_cli() {
